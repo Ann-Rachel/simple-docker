1cbe6a5eeaaf120dfe4b4e89579c2d9df8856551
Ann-Rachel <33717272+Ann-Rachel@users.noreply.github.com>
2021-08-08 08:32:30 +0200

    Update entrypoint_train.sh

--- entrypoint_train.sh
+++ entrypoint_train.sh
@@ -17,7 +17,7 @@ source /root/catkin_ws/devel/setup.bash
 export PYTHONPATH=//geometry2_ws/devel/lib/python3/dist-packages:${PYTHONPATH}
 source /root/.python_env/rosnav/bin/activate
 
-screen -dmS roslaunch bash -c "source ./ros.sh "$roslaunch""
+screen -dmS roslaunch bash -c "source ./docker/ros.sh "$roslaunch""
 screen -S roslaunch -X logfile screenlog_roslaunch.log
 screen -S roslaunch -X log
 sleep 10
@@ -25,7 +25,7 @@ sleep 10
 # python scripts/training/train_agent.py --agent MLP_ARENA2D
 
 #bash ./py_train.sh $trainmode $agent
-screen -dmS python_training bash -c "source ./py_train.sh "$python_train""
+screen -dmS python_training bash -c "source ./docker/py_train.sh "$python_train""
 screen -S python_training -X logfile screenlog_python_train.log
 screen -S python_training -X log
 sleep 4
